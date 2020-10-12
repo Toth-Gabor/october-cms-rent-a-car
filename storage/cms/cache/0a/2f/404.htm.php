a925b121aa8500271b169d52e555b5f821b5916c
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2020-10-12 18:52:11 +0200

    first commit

---
+++ storage/cms/cache/0a/2f/404.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5f84886532b4a311387501_4f50e89e83e053a9ac3a98116bc14a7aClass extends Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/cache/61/ae/footer.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5f84886545adf096446866_c1b5803c658cbf4568bcaf403a063eabClass extends Cms\Classes\PartialCode
+{
+
+}
